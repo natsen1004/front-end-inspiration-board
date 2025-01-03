f668b8bed2825f415c375329631b3620ffb63bb0
Natalie <natali3sen@gmail.com>
2025-01-03 14:46:02 -0800

    Created NewBoardForm and rendered on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import NewCardForm from './components/NewCardForm.jsx';
+import NewBoardForm from './components/NewBoardForm';
+import axios from 'axios';
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const convertFromApi = (apiCard) => {
+  const newCard = {
+    ...apiCard,
+  };
+  return newCard;
+};
+
+function App () {
+  const [cardData, setCardData] = useState([]);
+  const [boards, setBoards] = useState([]);
+
+  const cardAPIUrl = 'http://127.0.0.1:5000';
+  const boardAPIUrl = 'http://127.0.0.1:5000';
+
+  const createNewBoard = (newBoard) => {
+    axios.post(`${boardAPIUrl}/boards`, newBoard)
+      .then((response) => {
+        const createBoard = response.data.board;
+        setBoards((prevBoards) => [createBoard, ...prevBoards]);
+      })
+      .catch((error) => console.error('Error creating board:', error))
+  };
+
+  const handleSubmit = (cardData) => {
+    // next line needs to change after define the board API
+    axios.post(`${cardAPIUrl}/boards/1/cards`, cardData) 
+      .then(result => {
+        setCardData((prevCards) => [convertFromApi(result.data.card), ...prevCards]);
+      })
+      .catch((error) => console.log(error));
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="App">
+        <header className="App-header">
+          <h1>CREATE A BOARD AND CARD</h1>
+        </header>
+        <main>
+          <NewBoardForm createNewBoard={createNewBoard} />
 
-export default App
+          <NewCardForm handleSubmit={handleSubmit} />
+
+          <h3>Boards</h3>
+          <ul>
+            {boards.length > 0 ? (
+              boards.map((board, index) => (
+                <li key={index}>
+                  <strong>{board.title}</strong> by {board.owner}
+                </li>
+              ))
+            ) : (
+              <p>No boards available</p>
+            )}
+          </ul>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default App;
+
