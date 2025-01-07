17ad9e51352df9d55dc7a514c2f4270f58a5ef3a
Wei <qwbu22@gmail.com>
2025-01-06 18:00:28 -0800

    style: add simple  css for card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,34 @@
 // Card.jsx
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Card.css';
 
+// function Card({ card, handleLike, handleDelete }) {
+//   return (
+//     <li className="card-container">
+//       key={card.id}>
+//       <p>{card.message}</p>
+//       <p>{card.likes_count}💕</p>
+//       <button onClick={() => handleLike(card.id)}>Like</button>
+//       <button onClick={() => handleDelete(card.id)}>Delete</button>
+//     </li>
+//   );
+// }
 function Card({ card, handleLike, handleDelete }) {
   return (
-    <li key={card.id}>
-      <p>{card.message}</p>
-      <p>{card.likes_count}💕</p>
-      <button onClick={() => handleLike(card.id)}>Like</button>
-      <button onClick={() => handleDelete(card.id)}>Delete</button>
+    <li className="card-container">
+      <p className="card-message">{card.message}</p>
+      <p className="card-likes">{card.likes_count} 💕</p>
+      <div className="card-buttons">
+        <button onClick={() => handleLike(card.id)}>Like</button>
+        <button className="delete-btn" onClick={() => handleDelete(card.id)}>
+          Delete
+        </button>
+      </div>
     </li>
   );
 }
+
 
 
 Card.propTypes = {
